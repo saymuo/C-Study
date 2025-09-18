ea5744ccef25e3da9114e62c111dcef5018add4f
saymuo <hiharry10@naver.com>
2025-09-18 20:42:25 +0900

    SecondCommit

--- C#Study/Mainapp.cs
+++ C#Study/Mainapp.cs
@@ -7,7 +7,54 @@ namespace C_Study
     {
         static void Main(string[] args)
         {
-            
+            /*// 03.IntegralTypes
+            sbyte a = -10;
+            byte b = 40;
+
+            WriteLine($"a = {a}, b = {b}");
+
+            short c = -30000;
+            ushort d = 60000;
+
+            WriteLine($"c = {c}, d = {d}");
+
+            int e = -1000_0000; // 0이 7개
+            uint f = 3_0000_0000; // 0이 8개
+
+            WriteLine($"e = {e}, f = {f}");
+
+            long g = -5000_0000_0000; // 0이 11개
+            ulong h = 200_0000_0000_0000_0000; // 0이 18개
+
+            WriteLine($"g = {g}, h = {h}");*/
+
+            /*// 03.IntegerLiterals
+            byte a = 240;       // 10진수리터럴
+            WriteLine($"a = {a}");
+
+            byte b = 0b1111_0000; // 2진수리터럴
+            WriteLine($"b = {b}");
+
+            byte c = 0xF0;      // 16진수리터럴
+            WriteLine($"c = {c}");
+
+            uint d = 0x1234_abcd; // 16진수리터럴
+            WriteLine($"d = {d}");*/
+
+            /*// 03.signedUnsigned
+            byte a = 255;
+            sbyte b = (sbyte)a;
+
+            WriteLine($"a = {a}, b = {b}"); // b = -1*/
+
+            /*// 03.Overflow
+            uint a = uint.MaxValue;
+
+            WriteLine(a);
+
+            a = a + 1; // 오버플로우 발생
+
+            WriteLine(a);*/
         }
     }
 }
