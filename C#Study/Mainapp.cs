e2d5f2584fcf40a86026163f4c0e28662b33064f
saymuo <hiharry10@naver.com>
2025-09-18 20:22:12 +0900

    초기 커밋.

---
+++ C#Study/Mainapp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace C_Study
+{
+    internal class Mainapp
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
